a99e12420db7e8be74aa489547f11c9b69099869
Xianghui Liu <largepuma@gmail.com>
2011-10-09 18:15:11 +0800

    Edited geturl_my.py via GitHub

--- geturl_my.py
+++ geturl_my.py
@@ -142,20 +142,5 @@ if __name__ == '__main__':
     print "begin"
     process()
     print "end"
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+ 
     
